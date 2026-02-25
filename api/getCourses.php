6ec249407867a410779d1d77a8b42a3015a46ea7
Ralph <ralph@ralphwardlaw.co.uk>
2026-02-25 18:47:40 +0000

    json validation

--- api/getCourses.php
+++ api/getCourses.php
@@ -7,7 +7,11 @@ header("Content-Type: application/json; charset=utf-8");
 require_once "courses.php";
 
 if (isset($golfCourses)) {
-    echo json_encode($golfCourses);
+    $golfCourses = array_map(function ($course) {
+        return mb_convert_encoding($course, "UTF-8", "auto");
+    }, $golfCourses);
+
+    echo json_encode($golfCourses, JSON_UNESCAPED_UNICODE);
 } else {
     echo "{}";
 }
