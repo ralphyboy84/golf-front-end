5dcd2e7b347a2b732026cbee1f3021a21d9ff798
Ralph <ralph@ralphwardlaw.co.uk>
2026-03-05 16:44:36 +0000

    embedding google map

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,4 @@
 import { buildCard } from "../pages/components";
-import { abstractedFunction } from "../pages/tripBuilder";
 
 export function loadHome() {
   const app = document.getElementById("app");
@@ -8,9 +7,9 @@ export function loadHome() {
   <p class="mb-4 text-base">Welcome to the Scottish Golf Database - the most comprehensive resource for Scottish Golf on the internet</p>
   <p class="mb-4 text-base">Whether it is checking course availability, searching for open competitions, or just merely wanting to look at pictures of beautiful Scottish golf courses, we have everything you could ever need.</p>
   <div class="flex gap-2">
-    <a href="/courseDirectory" class="badge badge-primary h-auto text-center">547 Courses Loaded in the app</a>
+    <a href="/courseDirectory" class="badge badge-primary h-auto text-center">555 Courses Loaded in the app</a>
     <a href="/dayAvailability" class="badge badge-accent h-auto text-center">357 Courses With Online Booking</a>
-    <a href="/openSearcher" class="badge badge-info h-auto text-center">262 Courses With Open Competition Booking</a>
+    <a href="/openSearcher" class="badge badge-info h-auto text-center">263 Courses With Open Competition Booking</a>
   </div>
   `;
   app.innerHTML = buildCard(
@@ -19,4 +18,16 @@ export function loadHome() {
     content,
     "homePage",
   );
+
+  app.innerHTML += `
+  <div class="w-full max-w-2xl mx-auto">
+    <iframe 
+      src="https://www.google.com/maps/d/embed?mid=17GjIFtAKs18qYQN_WnmKnRaRIBnHHPE&ehbc=2E312F&noprof=1" 
+      class="w-full aspect-video rounded-lg shadow-md" 
+      style="border:0;" 
+      allowfullscreen="" 
+      loading="lazy">
+    </iframe>
+  </div>  
+  `;
 }
