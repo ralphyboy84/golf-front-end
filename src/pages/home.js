c0935b520464df7dd605017e7c17951fe4d46ea9
Ralph <ralph@ralphwardlaw.co.uk>
2026-03-08 21:54:19 +0000

    tidy ups

--- src/pages/home.js
+++ src/pages/home.js
@@ -7,9 +7,9 @@ export function loadHome() {
   <p class="mb-4 text-base">Welcome to the Scottish Golf Database - the most comprehensive resource for Scottish Golf on the internet</p>
   <p class="mb-4 text-base">Whether it is checking course availability, searching for open competitions, or just merely wanting to look at pictures of beautiful Scottish golf courses, we have everything you could ever need.</p>
   <div class="flex gap-2">
-    <a href="/courseDirectory" class="badge badge-primary h-auto text-center">555 Courses Loaded in the app</a>
-    <a href="/dayAvailability" class="badge badge-accent h-auto text-center">357 Courses With Online Booking</a>
-    <a href="/openSearcher" class="badge badge-info h-auto text-center">263 Courses With Open Competition Booking</a>
+    <a href="/courseDirectory" class="badge badge-primary h-auto text-center">557 Courses Loaded in the app</a>
+    <a href="/dayAvailability" class="badge badge-accent h-auto text-center">372 Courses With Online Booking</a>
+    <a href="/openSearcher" class="badge badge-info h-auto text-center">265 Courses With Open Competition Booking</a>
   </div>
   `;
   app.innerHTML = buildCard(
